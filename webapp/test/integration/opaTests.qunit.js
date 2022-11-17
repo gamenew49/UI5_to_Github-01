c75728e5e782bd909d1c7578cce5249cf61ddc3b
chkomsan <gamenew49@gmail.com>
2022-11-17 11:29:45 +0700

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zdemopo01/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zdemo_po_01/test/unit/controller/List.controller"
+], function () {
+	"use strict";
+});
